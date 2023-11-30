2539a347b9c01d1bf14dfb655a13d1b204de09f9
Ian Miller <ian@devpipeline.com>
2023-11-30 14:54:53 -0700

    bot created and styled, testing env variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
+import ChatBot from "./components/ChatBot";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div>Is this a bot?</div>
+      <ChatBot />
+    </div>
+  );
 }
 
 export default App;
